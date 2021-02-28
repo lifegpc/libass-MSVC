limkokhole/libass

cache: switch to faster hash function

libass/ass_utils.h
@@ -30,6 +30,7 @@
 #include <math.h>
 
 #include "ass.h"
+#include "wyhash.h"
 
 #ifndef SIZE_MAX
 #define SIZE_MAX ((size_t)-1)
@@ -160,32 +161,15 @@ static inline int double_to_d22(double x)
     return lrint(x * 0x400000);
 }
 
-#define FNV1_32A_INIT 0x811c9dc5U
-#define FNV1_32A_PRIME 16777619U
-
-static inline uint32_t fnv_32a_buf(void *buf, size_t len, uint32_t hval)
+static inline uint32_t fnv_32a_buf(const void *buf, size_t len, uint32_t hval)
 {
-    unsigned char *bp = (unsigned char *) buf;
-    size_t n = (len + 3) / 4;
-
-    switch (len % 4) {
-    case 0: do { hval ^= *bp++; hval *= FNV1_32A_PRIME; //-fallthrough
-    case 3:      hval ^= *bp++; hval *= FNV1_32A_PRIME; //-fallthrough
-    case 2:      hval ^= *bp++; hval *= FNV1_32A_PRIME; //-fallthrough
-    case 1:      hval ^= *bp++; hval *= FNV1_32A_PRIME;
-               } while (--n > 0);
-    }
-
-    return hval;
+    static const uint64_t _wyp[5] = {0xa0761d6478bd642full, 0xe7037ed1a0b428dbull, 0x8ebc6af09c88c6e3ull, 0x589965cc75374cc3ull, 0x1d8e4e27c47d124full};
+    return (uint32_t)wyhash(buf, len, hval, _wyp);
 }
+
 static inline uint32_t fnv_32a_str(const char *str, uint32_t hval)
 {
-    unsigned char *s = (unsigned char *) str;
-    while (*s) {
-        hval ^= *s++;
-        hval *= FNV1_32A_PRIME;
-    }
-    return hval;
+    return fnv_32a_buf(str, strlen(str), hval);
 }
 
 static inline int mystrtoi(char **p, int *res)

lifegpc/libass-MSVC

libass/ass_utils.h
@@ -30,6 +30,7 @@
 #include <math.h>
 
 #include "ass.h"
+#include "wyhash.h"
 
 #ifndef SIZE_MAX
 #define SIZE_MAX ((size_t)-1)
@@ -180,26 +181,12 @@ static inline int double_to_d22(double x)
     return lrint(x * 0x400000);
 }
 
-#define ASS_HASH_INIT 0x811c9dc5U
-#define FNV1_32A_PRIME 16777619U
+#define ASS_HASH_INIT 0U
 
-static inline uint32_t ass_hash_buf(const void *buf, size_t len, uint32_t hval)
+static inline uint64_t ass_hash_buf(const void *buf, size_t len, uint64_t hval)
 {
-    if (!len)
-        return hval;
-
-    const uint8_t *bp = buf;
-    size_t n = (len + 3) / 4;
-
-    switch (len % 4) {
-    case 0: do { hval ^= *bp++; hval *= FNV1_32A_PRIME; //-fallthrough
-    case 3:      hval ^= *bp++; hval *= FNV1_32A_PRIME; //-fallthrough
-    case 2:      hval ^= *bp++; hval *= FNV1_32A_PRIME; //-fallthrough
-    case 1:      hval ^= *bp++; hval *= FNV1_32A_PRIME;
-               } while (--n > 0);
-    }
-
-    return hval;
+    static const uint64_t _wyp[5] = {0xa0761d6478bd642full, 0xe7037ed1a0b428dbull, 0x8ebc6af09c88c6e3ull, 0x589965cc75374cc3ull, 0x1d8e4e27c47d124full};
+    return wyhash(buf, len, hval, _wyp);
 }
 
 static inline int mystrtoi(char **p, int *res)